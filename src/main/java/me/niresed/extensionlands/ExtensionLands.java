7784b677d483279c824734962259bac51f77cd33
Niresed <fuadsafarli686@gmail.com>
2023-04-25 20:59:37 +0400

    Ext

---
+++ src/main/java/me/niresed/extensionlands/ExtensionLands.java
@@ -0,0 +1,11 @@
+package me.niresed.extensionlands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import me.angeschossen.lands.api.land.LandWorld;
+
+public final class ExtensionLands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
