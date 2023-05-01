dc563b6df62e5c90874529989878a894f7ce12eb
Niresed <fuadsafarli686@gmail.com>
2023-05-01 16:35:02 +0400

    Ext

--- src/main/java/me/niresed/extensionlands/Utils/CheckLocation.java
+++ src/main/java/me/niresed/extensionlands/Utils/CheckLocation.java
@@ -19,7 +19,7 @@ public class CheckLocation {
         for (int i = 0; i < 6; i++){
             for (int s = 0; s < 6; s++){
                 if (Objects.equals(api.getLandByChunk(chunk.getWorld(), chunk.getX() + x, chunk.getZ() + z), player.getOwningLand())) {
-                    if (x >= -1 && x <= 1 && z >= -1 && z <= 1){
+                    if ((x >= -1 && x <= 1 && z >= -1 && z <= 1) && (x != 0 || z != 0)){
                         check = true;
                     }
                 } else if (api.getLandByChunk(chunk.getWorld(), chunk.getX() + x, chunk.getZ() + z) != null) {
