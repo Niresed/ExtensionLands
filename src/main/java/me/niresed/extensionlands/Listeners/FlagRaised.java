459f34c3cb7013c4d9dea5c13a35062260ad6402
Niresed <fuadsafarli686@gmail.com>
2023-05-02 16:48:04 +0400

    Ext

--- src/main/java/me/niresed/extensionlands/Listeners/FlagRaised.java
+++ src/main/java/me/niresed/extensionlands/Listeners/FlagRaised.java
@@ -8,6 +8,7 @@ import me.niresed.extensionlands.Main.ExtensionLands;
 import me.niresed.extensionlands.Utils.CheckLocation;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
@@ -24,11 +25,14 @@ public class FlagRaised implements Listener {
             return;
         }
         Land land = landPlayer.getOwningLand();
+        if (land == null) {
+            return;
+        }
         if (ev.getTag().contains("flagDataOfLandId:" + Objects.requireNonNull(landPlayer.getOwningLand()).getId())) {
             Location location = ev.getBlock().getLocation();
             if (CheckLocation.checkTheArea(location, api, landPlayer)){
-                assert land != null;
                 land.claimChunk(landPlayer, Objects.requireNonNull(Bukkit.getWorld("world")), location.getChunk().getX(), location.getChunk().getZ());
+                ev.getBlock().setType(Material.AIR);
             }
         }
     }
