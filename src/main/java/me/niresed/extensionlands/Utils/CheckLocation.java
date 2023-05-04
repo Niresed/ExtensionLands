04d29906983e4925fcd0099d206d35b71a4be71a
Niresed <fuadsafarli686@gmail.com>
2023-05-04 20:42:34 +0400

    Ext

--- src/main/java/me/niresed/extensionlands/Utils/CheckLocation.java
+++ src/main/java/me/niresed/extensionlands/Utils/CheckLocation.java
@@ -1,6 +1,7 @@
 package me.niresed.extensionlands.Utils;
 
 import me.angeschossen.lands.api.LandsIntegration;
+import me.angeschossen.lands.api.land.Land;
 import me.angeschossen.lands.api.player.LandPlayer;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
@@ -13,9 +14,11 @@ public class CheckLocation {
         int x = -3;
         int z = -3;
         boolean check = false;
+        Land land;
         for (int i = 0; i <= 6; i++) {
             for (int s = 0; s <= 6; s++){
-                if (Objects.equals(api.getLandByChunk(chunk.getWorld(), chunk.getX() + x, chunk.getZ() + z), player.getOwningLand())) {
+                land = api.getLandByChunk(chunk.getWorld(), chunk.getX() + x, chunk.getZ() + z);
+                if (Objects.equals(land, player.getOwningLand())) {
                     if ((x >= -1 && x <= 1 && z >= -1 && z <= 1) && (x != 0 || z != 0)){
                         check = true;
                     }
